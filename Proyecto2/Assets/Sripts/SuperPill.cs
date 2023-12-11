c59b4f73d9a320ab942139be28b8c44a84e61f1d
Santiago <chocs.whatever@gmail.com>
2023-12-11 00:41:37 -0600

    Se agrega objeto 3d externo; Se termina lógica para pasar de una escena a la otra; se agrega que los muros caigan al inicio de la escena

--- Proyecto2/Assets/Sripts/SuperPill.cs
+++ Proyecto2/Assets/Sripts/SuperPill.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SuperPill : MonoBehaviour
 {
     //Va a guardar el componente PlayerScript del jugador
     private PlayerController playerScript;
+
+    public int time; 
+    private float timeTrans;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +26,21 @@ public class SuperPill : MonoBehaviour
     //Para indicarle que cuando colisionen se cambie la variable invencible
     // a verdadero
     void OnTriggerEnter(Collider other){
-        if(other.CompareTag("Player")){
+        if(other.CompareTag("Player")){// revisar que el objeto con el que este colisionando sea el jugador
             playerScript.isInv = true;
+            // Comienza la coroutine que espera 5 segundos y luego cambia la escena
+            StartCoroutine(WaitAndLoadScene());
+             gameObject.GetComponent<Renderer>().enabled = false;
         }
+    }
 
+     // Coroutine para esperar y cargar la escena
+    IEnumerator WaitAndLoadScene()
+    {
+        // Espera 5 segundos
+        yield return new WaitForSeconds(5f);
+
+        // Carga la escena "FinalScene"
+        SceneManager.LoadScene("FinalScene");
     }
 }
